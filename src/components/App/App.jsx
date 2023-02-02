c45f00472124d59da75d7f5b018f9ef72ab305a3
Serhei Honcharov <mrsedmor@gmail.com>
2023-02-02 22:41:37 +0200

    fixed initial contacts fetch

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -25,23 +25,16 @@ function getFilteredContacts(filter, contacts) {
 }
 
 export default function App() {
-  const [contacts, setContacts] = useState([]);
-  const [filter, setFilter] = useState('');
-  const firstRun = useRef(true);
-
-  useEffect(() => {
-    let contacts = api.restore();
+  const [contacts, setContacts] = useState(() => {
+    const contacts = api.restore();
     if (contacts.length === 0) {
-      contacts = sortContacts(initialContacts);
+      return sortContacts(initialContacts);
     }
-    setContacts(contacts);
-  }, []);
+    return contacts;
+  });
+  const [filter, setFilter] = useState('');
 
   useEffect(() => {
-    if (firstRun.current) {
-      firstRun.current = false;
-      return;
-    }
     api.store(contacts);
   }, [contacts]);
 
