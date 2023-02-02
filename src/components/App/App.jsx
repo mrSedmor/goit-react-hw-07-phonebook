4fbe5fb8c2dc67f9674d570def72f13cf9e1d745
Serhei Honcharov <mrsedmor@gmail.com>
2023-02-02 22:46:27 +0200

    fixed

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
