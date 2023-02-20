f202b3fe6aed0003530edd546af1a8f162f55cea
Serhei Honcharov <mrsedmor@gmail.com>
2023-02-20 21:46:49 +0200

    Filter.jsx; added clear button

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,4 +1,6 @@
+import { useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { FaBackspace } from 'react-icons/fa';
 import { getFilter } from 'redux/selectors';
 import { setFilter } from 'redux/filterSlice';
 import css from './filter.module.css';
@@ -7,19 +9,28 @@ export default function Filter() {
   const dispatch = useDispatch();
   const filter = useSelector(getFilter);
 
-  function handleChange({ currentTarget: { value } }) {
+  const handleChange = useCallback(({ currentTarget: { value } }) => {
     dispatch(setFilter(value));
-  }
+  });
+
+  const handleClear = useCallback(() => {
+    dispatch(setFilter(''));
+  });
   return (
     <label className={css.field}>
       <span className={css.label}>Find contacts by name</span>
-      <input
-        className={css.input}
-        type="text"
-        name="filter"
-        onChange={handleChange}
-        value={filter}
-      />
+      <div className={css.inputWrapper}>
+        <input
+          className={css.input}
+          type="text"
+          name="filter"
+          onChange={handleChange}
+          value={filter}
+        />
+        <button type="button" className={css.iconButton} onClick={handleClear}>
+          <FaBackspace />
+        </button>
+      </div>
     </label>
   );
 }
