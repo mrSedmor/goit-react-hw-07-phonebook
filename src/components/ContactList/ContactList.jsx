2f4cf1402f46b66b6753555589da70da0bc94cc6
Serhei Honcharov <mrsedmor@gmail.com>
2023-02-23 18:20:09 +0200

    refactor: move getFilteredContacts to selectors.js

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,27 +1,13 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteContact } from 'redux/contactsSlice';
-import { getContacts } from 'redux/selectors';
-import { getFilter } from 'redux/selectors';
+import { getFilteredContacts } from 'redux/selectors';
 
 import css from './contact-list.module.css';
 import sharedCss from 'shared.module.css';
 
-function getFilteredContacts(filter, contacts) {
-  if (filter === '') {
-    return contacts;
-  }
-
-  const normalizedFilter = filter.toLocaleLowerCase();
-  return contacts.filter(({ name }) =>
-    name.toLocaleLowerCase().includes(normalizedFilter)
-  );
-}
-
 export default function ContactList() {
   const dispatch = useDispatch();
-  const contacts = useSelector(getContacts);
-  const filter = useSelector(getFilter);
-  const filteredContacts = getFilteredContacts(filter, contacts);
+  const filteredContacts = useSelector(getFilteredContacts);
 
   return (
     <ul className={css.list}>
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,2 +1,14 @@
-export const getContacts = state => state.contacts;
-export const getFilter = state => state.filter;
+export const getContacts = ({ contacts }) => contacts;
+
+export const getFilter = ({ filter }) => filter;
+
+export const getFilteredContacts = ({ contacts, filter }) => {
+  if (filter === '') {
+    return contacts;
+  }
+
+  const normalizedFilter = filter.toLocaleLowerCase();
+  return contacts.filter(({ name }) =>
+    name.toLocaleLowerCase().includes(normalizedFilter)
+  );
+};
